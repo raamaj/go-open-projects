442489c31b308d500f42355384e831fac12bb792
Faqihyugos <faqihyugos@gmail.com>
2024-01-12 16:53:51 +0700

    helper model and tx for commitorRollback

---
+++ golang-restful-api/helper/model.go
@@ -0,0 +1,13 @@
+package helper
+
+import (
+	"golang-restful-api/model/domain"
+	"golang-restful-api/model/web"
+)
+
+func ToCategoryResponse(category domain.Category) web.CategoryResponse {
+	return web.CategoryResponse{
+		Id:   category.Id,
+		Name: category.Name,
+	}
+}
